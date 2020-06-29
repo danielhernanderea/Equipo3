a892c867c47dc471994f33c754e34ef3ac194a73
Nadia Buendia <nadiabuendia@gmail.com>
2020-06-30 00:25:36 +0200

    Version inicial API REST

--- server.js
+++ server.js
@@ -8,4 +8,57 @@ var path = require('path');
 
 app.listen(port);
 
+var movimientosJSON = require('./movimientosv2.json');
+var bodyparser = require('body-parser');
+app.use(bodyparser.json());
+
 console.log('todo list RESTful API server started on: ' + port);
+
+app.get('/',function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+//app.get('/',function(req, res) {
+  //res.send("hola mundo desde JS"));
+//});
+
+app.post('/',function(req, res) {
+  res.send("Hemos recibido su petición post");
+});
+
+app.put('/',function(req, res) {
+  res.send("Hemos recibido su petición put");
+});
+
+app.delete('/',function(req, res) {
+  res.send("Hemos recibido su petición de borrado");
+});
+
+app.get('/Clientes/:idcliente',function(req, res) {
+  res.send("Aquí tiene al cliente número: " + req.params.idcliente);
+});
+
+app.get('/v1/Movimientos',function(req, res) {
+  res.sendFile('movimientosv1.json');
+});
+
+app.get('/v2/Movimientos',function(req, res) {
+  res.json(movimientosJSON);
+});
+
+app.get('/v2/Movimientos/:id',function(req, res) {
+  console.log(req.params.id);
+  res.send(movimientosJSON[req.params.id - 1]);
+});
+
+app.get('/v2/MovimientosQuery/',function(req, res) {
+  console.log(req.query);
+  res.send("Se recibió el query");
+});
+
+app.post('/v2/Movimientos/',function(req, res) {
+  var nuevo = req.body
+  nuevo.id = movimientosJSON.length + 1
+  movimientosJSON.push(nuevo)
+  res.send("Movimiento dado de alta")
+});
