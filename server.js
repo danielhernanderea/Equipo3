293a0d0c731d00fb61382d96339089d57a48550c
Nadia Buendia <nadiabuendia@gmail.com>
2020-07-02 22:49:45 +0200

    Version Dockerizada

--- server.js
+++ server.js
@@ -1,64 +1,59 @@
 //version inicial
 
 var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
-
+app = express(),
+port = process.env.PORT || 3000;
 var path = require('path');
-
 app.listen(port);
+var bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
 
 var movimientosJSON = require('./movimientosv2.json');
-var bodyparser = require('body-parser');
-app.use(bodyparser.json());
 
 console.log('todo list RESTful API server started on: ' + port);
 
-app.get('/',function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
-});
+})
 
-//app.get('/',function(req, res) {
-  //res.send("hola mundo desde JS"));
-//});
-
-app.post('/',function(req, res) {
+app.post('/', function(req, res) {
   res.send("Hemos recibido su petición post");
-});
+})
 
-app.put('/',function(req, res) {
+app.put('/', function(req, res) {
   res.send("Hemos recibido su petición put");
-});
+})
 
-app.delete('/',function(req, res) {
-  res.send("Hemos recibido su petición de borrado");
-});
+app.delete('/', function(req, res) {
+  res.send("Hemos recibido su petición delete");
+})
 
-app.get('/Clientes/:idcliente',function(req, res) {
-  res.send("Aquí tiene al cliente número: " + req.params.idcliente);
-});
+app.get('/Clientes/:idcliente', function(req, res) {
+  res.send("Aquí tiene al cliente número " + req.params.idcliente);
+})
 
-app.get('/v1/Movimientos',function(req, res) {
-  res.sendFile('movimientosv1.json');
-});
+app.get('/v1/Movimientos', function(req, res) {
+  res.sendfile('movimientosv1.json');
+})
 
-app.get('/v2/Movimientos',function(req, res) {
+app.get('/v2/Movimientos', function(req, res) {
   res.json(movimientosJSON);
-});
+})
 
-app.get('/v2/Movimientos/:id',function(req, res) {
+app.get('/v2/Movimientos/:id', function(req, res) {
   console.log(req.params.id);
   res.send(movimientosJSON[req.params.id - 1]);
-});
+})
 
-app.get('/v2/MovimientosQuery/',function(req, res) {
+app.get('/v2/MovimientosQuery', function(req, res) {
   console.log(req.query);
-  res.send("Se recibió el query");
-});
+  res.send("Se recibio el query");
+})
 
-app.post('/v2/Movimientos/',function(req, res) {
+app.post('/v2/Movimientos', function(req, res) {
   var nuevo = req.body
   nuevo.id = movimientosJSON.length + 1
   movimientosJSON.push(nuevo)
   res.send("Movimiento dado de alta")
-});
+})
