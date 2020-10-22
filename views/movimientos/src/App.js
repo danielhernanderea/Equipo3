59162885834991e7d83cc626a2bc29edfb6123ca
Uziel Linares <uzielnmtz@gmail.com>
2020-10-22 12:33:54 -0500

    cambia la consulta dentro de la app

--- views/movimientos/src/App.js
+++ views/movimientos/src/App.js
@@ -8,7 +8,18 @@ import TableRow from '@material-ui/core/TableRow';
 import TableHead from '@material-ui/core/TableHead';
 import {getOperations} from "./client.js";
 import Paper from '@material-ui/core/Paper';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
 // logo
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
 
 
 class OperationsListView extends React.Component {
@@ -16,6 +27,7 @@ class OperationsListView extends React.Component {
         loggedUser: 'prueba@bbva.com',
         records: []
     }
+    
 
     handleMovements = (user) => {
         getOperations(user, (response) => {
@@ -23,10 +35,15 @@ class OperationsListView extends React.Component {
         })
     }
 
+  componentDidMount() {
+      let localhost = 'http://127.0.0.1:3000'
+      fetch(localhost + '/Movimientos/' + this.state.loggedUser)
+          .then(response => response.json())
+          .then(data => this.setState({ records: data.movimiento }))
+  }
+
   render() {
       let columnNames = ["Operación", "Tipo", "Fecha de Operación", "Costo", "Precio de compra", "Descripción"]
-      // let records = getOperations(this.state.loggedUser)
-     this.handleMovements(this.state.loggedUser) 
     console.log(this.state.records)
     return (
       <TableContainer component={Paper}>
@@ -40,12 +57,12 @@ class OperationsListView extends React.Component {
         </TableHead>
           {this.state.records.map((r, idx) => (
               <TableRow key={idx}>
-                <TableCell align="left">{r.tipooperacion}</TableCell>
-                  <TableCell align="left">{r.idcripto}</TableCell>
-                  <TableCell align="left">{r.fecha}</TableCell>
-                  <TableCell align="left">{r.montogastado}</TableCell>
-                  <TableCell align="left">{r.preciocompra}</TableCell>
-                  <TableCell align="left">{r.descripcion}</TableCell>
+                <StyledTableCell align="left">{r.tipooperacion}</StyledTableCell>
+                  <StyledTableCell align="left">{r.idcripto}</StyledTableCell>
+                  <StyledTableCell align="left">{r.fecha}</StyledTableCell>
+                  <StyledTableCell align="left">{r.montogastado}</StyledTableCell>
+                  <StyledTableCell align="left">{r.preciocompra}</StyledTableCell>
+                  <StyledTableCell align="left">{r.descripcion}</StyledTableCell>
               </TableRow>
           ))}
         <TableBody>
